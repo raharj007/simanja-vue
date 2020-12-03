c4c430cf48bea28ba21443bb5340ed1cdab815ef
Adetiya Bagus Nusantara <bagus.nusantara313@gmail.com>
2020-12-03 23:10:43 +0700

    edit layout simanja

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,59 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Simanja UI</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>SIMANJA</v-toolbar-title>
     </v-app-bar>
-    <v-navigation-drawer app>
-      Side Menu
-    </v-navigation-drawer>
+    <v-navigation-drawer app> Side Menu </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
-     <v-footer app>
-      Footer
-    </v-footer>
+    <v-footer app> Footer </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
